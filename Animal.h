6ce7f8059589251e3580c92b3be6fd26a301382d
haniabaher16007635 <haniabaher6@gmail.com>
2025-11-12 15:09:34 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,7 +4,38 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal{
+    
+   private:
+   string name;
+   int age;
+   bool isHungry;
+public :
+void display(){
+   cout<< name<< endl;
+   cout<< age << endl;
+}
+void feed(){
+   if(isHungry==true){
+       cout<<"Feed the animal"<<endl;
+       }
+       else cout<<"Do not feed the animal"<<endl;
+    
+}
+ Animal(){
+     name=" ";
+     age=0;
+     isHungry=false;
+ }
+ Animal(string n, int a, bool h){
+     name=n;
+     age=a;
+     isHungry=h;
+ }
+     
+   ~Animal(){}; 
+    
+};
 
 
 
