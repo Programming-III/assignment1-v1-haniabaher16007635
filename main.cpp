4ef22eb3f9a3849110f0059eb0f27781057aea2a
haniabaher16007635 <haniabaher6@gmail.com>
2025-11-12 15:29:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,11 @@ int main() {
     Animal a("Lion", 5, true);
     Animal b("Parrot", 2, false);
     Animal c("Snake",3,true);
-    
+    a.display();
+    b.display();
+    c.display();
+    Visitor v1("Sarah Ali", 3 );
+    v1.displayInfo();
     
     return 0;
 }
