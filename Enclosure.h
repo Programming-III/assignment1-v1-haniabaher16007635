4b52df6921576df70ea5db389f3ee77c095baede
haniabaher16007635 <haniabaher6@gmail.com>
2025-11-12 15:14:49 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,7 +3,41 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure{
+    private:
+    Animal* a;
+    int capacity;
+    int currenCount;
+    public:
+    void addAnimal(Animal * a){
+        Animal*b = new Animal[currenCount+1];
+        for(int i=0; i<currenCount; i++){
+            b[i]=a[i];
+        }
+        b[currenCount]=a;
+        currenCount++;
+        Animal*t=a;
+        a=b;
+        delete[] t;
+    }
+    void displayAnimals(){
+        for(int i=0; i<currenCount; i++){
+            a[i].display();
+        }
+    }
+Enclosure(){
+  a=NULL;
+  capacity = 0;
+  currentCount=0;
+}
+Enclosure(Animal* a, int ca, int cu){
+  a=new Animal[currentCount];
+  capacity=ca;
+  current=cu;
+}
+        
+    
+};
 
 
 
